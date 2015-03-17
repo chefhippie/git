88b1417927608a9d77d0202dd960544b39193c20
Thomas Boerger <tboerger@suse.de>
2015-03-17 11:46:33 +0100

    Refresh the repos after added

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,7 +26,7 @@ when "suse"
     key node["git"]["zypper"]["key"]
     title node["git"]["zypper"]["title"]
 
-    action :add
+    action [:add, :refresh]
 
     only_if do
       node["git"]["zypper"]["enabled"]
